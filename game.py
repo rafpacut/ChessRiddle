58e06563d2dfc150085338f15c464b5730e1a163
rafal <rafpacut@gmail.com>
2019-01-18 21:46:05 +0100

    Display white/black knight around

--- game.py
+++ game.py
@@ -2,6 +2,45 @@ import pygame
 import time
 from drawBoard import *
 
+isOccupiedMatrix = [ [0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0] ]
+
+knightWhiteImg = pygame.image.load("knightW.png")
+knightBlackImg = pygame.image.load("knightB.png")
+
+currentColor = "white"
+
+def printOccupationMatrix():
+    for col in isOccupiedMatrix:
+        print(col)
+    print()
+
+def changeColor():
+    global currentColor
+    if currentColor == "white":
+        currentColor = "black"
+    else:
+        currentColor = "white"
+    
+    
+
+def moveKnight(x, y):
+    isOccupiedMatrix[y][x] = 1
+    if currentColor == "white":
+        currentKnightImg = knightWhiteImg
+    else:
+        currentKnightImg = knightBlackImg
+
+    game_display.blit(currentKnightImg, (x*75, y*75))
+    changeColor()
+
+
 
 pygame.init()
 
@@ -14,7 +53,7 @@ while running:
             mousePos = pygame.mouse.get_pos()
             x = mousePos[0] // 75
             y = mousePos[1] // 75
-            pygame.draw.rect(game_display, teal, (x * 50 * 1.5, y * 50 * 1.5, block_width, block_height))
+            moveKnight(x,y)
             pygame.display.update() 
             time.sleep(0.03)
     board_draw()
